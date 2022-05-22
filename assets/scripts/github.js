14f5e9c7a4789c167b3862cd28389eab601b31f7
nullnyat <nullnyat@outlook.com>
2022-05-23 04:46:32 +0900

    add dark mode

--- assets/scripts/github.js
+++ assets/scripts/github.js
@@ -2,6 +2,10 @@ window.onload = () => {
     getRepos();
 };
 
+const theme = localStorage.getItem('theme') === 'dark'
+    ? '&hide_border=ture&bg_color=2F343D&title_color=96CCE7&text_color=B0CBE7'
+    : ''
+
 function getRepos() {
     fetch('https://api.github.com/users/nullnyat/repos',{cache:'no-cache'})
         .then((res) => res.json())
@@ -10,7 +14,7 @@ function getRepos() {
             json.forEach((repo) => {
                 const a = document.createElement('a');
                 const img = document.createElement('img');
-                img.src = `https://gh-card.dev/repos/${repo.full_name}.svg`;
+                img.src = `https://github-readme-stats.vercel.app/api/pin/?username=nullnyat&repo=${repo.name}${theme}`;
                 a.href = repo.html_url;
                 a.append(img);
                 list.appendChild(a);
